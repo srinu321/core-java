5bd0db4c22d4ce6440fdad72a7c92baf05a4a124
srinivas4321 <chinna.sri0@gmail.com>
2018-09-29 22:46:10 -0400

    first program

---
+++ hello-world/src/main/java/com/sri/app/first/program/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.sri.app.first.program;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String firstName = "Srinivas Goud";
+		String LastName = "Thadakapally";
+		String fullName = firstName.concat(" ").concat(LastName);		
+		System.out.println(fullName);
+	}
+
+}
